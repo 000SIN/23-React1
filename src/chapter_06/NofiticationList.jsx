70cea4ed3aeba479958144b71910d63f25ef1fa4
000SIN <sin.dev200@gmail.com>
2023-04-06 12:33:14 +0900

    6주차

--- src/chapter_06/NofiticationList.jsx
+++ src/chapter_06/NofiticationList.jsx
@@ -5,11 +5,11 @@ const reservedNotifications = [
     {
         id: 1,
         message: "안녕하세요, 오늘의 일정을 알려드립니다."
-    }
+    },
     {
         id: 2,
         message: "점심식사 시간 입니다."
-    }
+    },
     {
         id: 3,
         message: "이제 곧 미팅이 시작됩니다."
