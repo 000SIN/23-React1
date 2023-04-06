ac1db18aa44265b7df9e545354931450961a11fb
000SIN <sin.dev200@gmail.com>
2023-04-06 11:19:43 +0900

    6주차

--- src/chapter_05/CommentList.jsx
+++ src/chapter_05/CommentList.jsx
@@ -1,9 +1,28 @@
 import React from "react";
+import React from "./Comment";
 
-function Comment(props) {
+const comments = [
+    {
+        name : "신동욱",
+        comment: "안녕하세요. 신동욱입니다."
+    },
+    {
+        name : "신동욱2",
+        comment: "안녕하세요. 신동욱2입니다."
+    },
+    {
+        name : "신동욱3",
+        comment: "안녕하세요. 신동욱3입니다."
+    },
+]
+/function Comment(props) {
     return (
         <div>
-            <Comment/>
+          {comments.mpa((foo) => {
+            return (
+                <Comment name={foo.name} comment={foo.comment}/>
+            )
+          })}
         </div>
     )
 }
