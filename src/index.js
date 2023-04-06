8a0fdc7dba277f32cec6e78720e431b7e7dc387a
000SIN <sin.dev200@gmail.com>
2023-04-06 12:29:25 +0900

    6주차

--- src/index.js
+++ src/index.js
@@ -5,11 +5,13 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Library from './chapter_03/Library';
 import Clock from './chapter_04/Clock';
+import CommentLsit from './chapter_05/CommentList';
+import NotificationList from './chapter_06/NofiticationList';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <CmmentList/>
+    <NotificationList/>
   </React.StrictMode>
 );
 
