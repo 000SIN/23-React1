224d1bca13bea255ba240565116a64759e3abb6a
000SIN <sin.dev200@gmail.com>
2023-04-20 13:02:23 +0900

    7

--- src/chapter_07/Accomodate.jsx
+++ src/chapter_07/Accomodate.jsx
@@ -1,24 +1,37 @@
-import React, { useState, useEffect} from "react";
+import React, {useState, useEffect} from "react";
 import useCounter from "./useCounter";
 
 const MAX_CAPACITY = 10;
 
-function Acoommdate(prps) {
-    const [isFull, setIsFull] = useState(false);
-    const [count, increasCount, decresaseCount] = useCounter(0);
+function Accommodate(props){
+  const [isFull, setIsFull] = useState(false);
+  const [count, increaseCount, decreaseCount] = useCounter(0);
 
-    useEffect(() => [
-        console.log("===================");
-        console.log("useEffect() is called");
-        console.log(`isFull : ${isFull}`);
+  useEffect(() => {
+    console.log("=====================");
+    console.log("useEffect() is called.");
+    console.log(`isFull: ${isFull}`);
+  });
 
-    ]);
+  useEffect(() => {
+    setIsFull(count >= MAX_CAPACITY);
+    console.log(`Current count value: ${count}`);
+  }, [count]);
+  
+  return(
+    <div style={{padding:16}}>
+      <p>{`총 ${count}명 수용했습니다.`}</p>
 
-    useEffect(() => [
-        setIsFull(count >= MAX_CAPACITY);
-        console.log(`Current count value: ${count}`);
-    ], [count]);
+      <button onClick={increaseCount} disabled={isFull}>
+        입장
+      </button>
+      <button onClick={decreaseCount}>
+        퇴장
+      </button>
 
-    return (
-        <div style=
-    )
+      {isFull && <p style={{color: "red"}}>정원이 가득찼습니다.</p>}
+    </div>
+  );
+}
+
+export default Accommodate;
