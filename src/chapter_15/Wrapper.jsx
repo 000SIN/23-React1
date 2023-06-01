0fef52802431829672dc9cad29c1f4343ea05910
000SIN <sin.dev200@gmail.com>
2023-06-01 11:50:05 +0900

    14

--- src/chapter_15/Wrapper.jsx
+++ src/chapter_15/Wrapper.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import styled from "styled-components";
 
-const Wrapper = styled.div
+const Wrapper = styled.div`
     padding: 1em;
     background: red;
-;
+`;
 
-const Tittle = styled.h1
+const Tittle = styled.h1`
     font-size: 1.5em;
     color: white ;
     text-align: center;
-;
+`;
     
     function MainPage(props){
         return (
